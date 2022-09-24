4c947a6e68c65e76a44e014216224cdcd266738d
Jen Keane <zenbuffy@gmail.com>
2022-09-24 10:51:16 +0100

    Remove incorrect images
    Update to remove some incorrect images

--- quote_to_image.php
+++ quote_to_image.php
@@ -3,7 +3,6 @@
 // this script turns quotes from books into images for use in a Kindle clock.
 // Jaap Meijers, 2018
 
-
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 ini_set('max_execution_time', 3000);
